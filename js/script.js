880d1f9277c04d11fd04d6caf70f7f356c325ac7
Noman Khan <101161473+thenomankhan@users.noreply.github.com>
2022-12-02 19:20:10 +0600

    footer,slider update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.video_slider').slick({
+        slidesToShow: 5,
+        arrows:false,
+        centerMode:true,
+        centerPadding:"60px",
+        // focusOnSelect:true,
+        // asNavFor: '.slider-for',
+        slideToScroll: 1,
+      });
+})
